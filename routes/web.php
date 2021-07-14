3525a1315952e733e76c9a37eb946551cb6f0772
Rozi666 <86703611+Rozi666@users.noreply.github.com>
2021-07-14 21:47:54 +0700

    Rozi
    Mbenakne route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('template.about_us.about_us');
-});
-// Route::get('/', 'AboutController@index');
+// Route::get('/', function () {
+//     return view('template.about_us.about_us');
+// });
+Route::get('/',[AboutController::class, 'index']);
