2ef10de5105eee127fd1af64a34b5ad4ef4c13f1
FathurRozi <86703611+Rozi666@users.noreply.github.com>
2021-08-20 22:49:19 +0700

    Fathur Rozi
    List Mobil

--- app/Http/Controllers/Template/MobilController.php
+++ app/Http/Controllers/Template/MobilController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers\Template;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class MobilController extends Controller
 {
     public function index()
     {
-        return view('template.mobil.mobil');
+                // mengambil data dari table mobil
+		$mobil = DB::table('mobil')->paginate(10);
+ 
+        // mengirim data mobil ke view index
+        return view('template.mobil.mobil',['mobil' => $mobil]);
     }
+
+    // public function mobil()
+	// {
+    // 	        // mengambil data dari table mobil
+	// 	$mobil = DB::table('mobil')->paginate(10);
+ 
+    // 	        // mengirim data mobil ke view index
+	// 	return view('template.mobil.mobil',['mobil' => $mobil]);
+ 
+	// }
 }
